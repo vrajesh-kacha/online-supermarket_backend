aaf55eee649402a7869cb503c4f5d4a492ae0960
vrajesh-kacha <kachavrajesh94@gmail.com>
2024-10-17 17:24:26 +0530

    modify index.js file

--- index.js
+++ index.js
@@ -28,9 +28,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-app.listen(process.env.PORT, () => {
-  console.log(`SEVER IS RUNNING on ${process.env.PORT}`);
-});
+
 
 connectiondb();
 
