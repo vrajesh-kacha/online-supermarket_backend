9dbd7cc3e499ac5b8e3acdbb07a097bb746a01fd
vrajesh-kacha <kachavrajesh94@gmail.com>
2024-10-29 10:02:27 +0530

    modilfy code of payment gateway

--- controller/filehandling.js
+++ controller/filehandling.js
@@ -2,7 +2,11 @@ import { v2 as cloudinary } from "cloudinary";
 const uploadOnCloudinary = (fileBuffer) => {
   return new Promise((resolve, reject) => {
     const uploadStream = cloudinary.uploader.upload_stream(
-      { resource_type: "image" },
+      { resource_type: "image",
+        width: 250,
+        height: 200,
+        crop: "fit"
+      },
       (error, result) => {
         if (error) {
           reject(new Error("Cloudinary upload failed: " + error.message));
